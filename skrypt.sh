40e0850495654ebc9b65393d567d29ba82f8c551
Bartosz Kurach <dsw51787@student.dsw.edu.pl>
2024-05-26 00:32:19 +0200

    Dodano automatyczne tworzenie 100 plików logx.txt

--- skrypt.sh
+++ skrypt.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+logs()
+{
+  for ((i=1; i<=100; i++)); do
+    filename="log$i.txt"
+    echo "File name: $filename" >> $filename
+    echo "Created by script: $0" >> $filename
+    echo "Date: $(date)" >> $filename
+  done
+}
+
 case "$1" in
   --date)
       date ;;
+  --logs)
+      logs ;;
 esac
